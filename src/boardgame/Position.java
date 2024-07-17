9678c09526fbbb5e8ef2f65198491d2073aeea98
Guilherme Dores <guilhermezdores@gmail.com>
2024-07-17 16:05:58 -0300

    chess layer and printing the board

---
+++ src/boardgame/Position.java
@@ -0,0 +1,41 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	//constructor
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	//methods
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	//getter setters
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	
+	
+	
+	
+}
