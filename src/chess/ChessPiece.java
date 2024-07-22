b394b99000eb2ec6039777bab491505ff89f1114
Guilherme Dores <guilhermezdores@gmail.com>
2024-07-22 20:49:30 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -24,4 +24,8 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}	
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position); 
+	}
 }
